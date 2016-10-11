098ad106397775b4573f0cca8b32a26e5de0ec75
Bujail <wwwbujail@gmail.com>
2016-10-11 22:26:07 +0530

    Bug fix

--- systemdetails.py
+++ systemdetails.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 import subprocess
+print("\n System Name:\n")
+subprocess.call("uname -a",shell=True)
+print("\n Disk Usage\n")
 subprocess.call("df -h",shell=True)
+print("\n /tmp Files:\n")
+subprocess.call("ls -la /tmp",shell=True)
+print("\n")
